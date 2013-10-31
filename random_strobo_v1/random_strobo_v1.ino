460880eb6b601e2947766091b57fe7a0efad1825
Tõnu <tonutunnel@gmail.com>
2013-10-31 16:28:50 +0200

    versioon 2
    Parandused. Arduino Leonardo on nüüd käepärast ja sain laivis katsetada.
    Mingi kala on veel sellega et leonardo ei pane koodi käima enne kui
    arduino soft on taga.

--- random_strobo_v1/random_strobo_v1.ino
+++ random_strobo_v1/random_strobo_v1.ino
@@ -13,15 +13,15 @@ http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
 
 
 //declare constants
-#define FLASH 20           // sähvatuse kestus (ms)
-#define MIN 100            // minimaalne delay sähvatuste vahel (ms)
+#define FLASH 10           // sähvatuse kestus (ms)
+#define MIN 10            // minimaalne delay sähvatuste vahel (ms)
 #define MAX 3000           // maksimaalne delay sähvatuste vahel (ms)
-#define pinYks 3      // esimene TIP -> led
-#define pinKaks 4      // teine TIP -> led
-#define pinKolm 5      // kolmas TIP -> led
+
+int led1 = 3;
+int led2 = 5;
+int led3 = 6;
 
 // Variables will change:
-int ledState = LOW;             // ledState used to set the LED
 long prevMillis1 = 0;        // will store last time LED was updated
 long prevMillis2 = 0;
 long prevMillis3 = 0;
@@ -33,13 +33,23 @@ long random3;              // kolmas aken genereeritud delay
 
 
 void setup(){
-  Serial.begin(9600);
+ // Serial.begin(9600);
   randomSeed(analogRead(0));
   
-  //genereerime esimesed rändom delay'd
-  random1 = random(MIN, MAX);
-  random2 = random(MIN, MAX);
-  random3 = random(MIN, MAX);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  
+  //esialgu kustu:
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  
+  
+//  //genereerime esimesed rändom delay'd
+//  random1 = random(MIN, MAX);
+//  random2 = random(MIN, MAX);
+//  random3 = random(MIN, MAX);
 }
 
 
@@ -54,12 +64,12 @@ void loop() {
 
   
   if(curMillis1 - prevMillis1 > random1) {
-    analogWrite(pinYks, 255);
+    digitalWrite(led1, LOW); //põleb
     delay(FLASH);
-    analogWrite(pinYks, 0);
+    digitalWrite(led1, HIGH); //kustu
     
-    Serial.println("random1:");  // for debugging
-    Serial.println(random1);
+//    Serial.println("random1:");  // for debugging
+//    Serial.println(random1);
     
     // save the last time you blinked the LED 
     prevMillis1 = curMillis1;   
@@ -68,12 +78,12 @@ void loop() {
   }
   
     if(curMillis2 - prevMillis2 > random2) {
-    analogWrite(pinKaks, 255);
+    digitalWrite(led2, LOW); //põleb
     delay(FLASH);
-    analogWrite(pinKaks, 0);
+    digitalWrite(led2, HIGH); //kustu
     
-    Serial.println("random2:");  // for debugging
-    Serial.println(random2);
+//    Serial.println("random2:");  // for debugging
+//    Serial.println(random2);
     
     // save the last time you blinked the LED 
     prevMillis2 = curMillis2;   
@@ -82,18 +92,19 @@ void loop() {
   }
   
     if(curMillis3 - prevMillis3 > random3) {
-    analogWrite(pinKolm, 255);
+    digitalWrite(led3, LOW); //põleb
     delay(FLASH);
-    analogWrite(pinKolm, 0);
+    digitalWrite(led3, HIGH); //kustu
     
-    Serial.println("random3:");  // for debugging
-    Serial.println(random3);
+//    Serial.println("random3:");  // for debugging
+//    Serial.println(random3);
     
     // save the last time you blinked the LED 
     prevMillis3 = curMillis3;   
     //uus rändom delay
     random3 = random(MIN, MAX);
   }
+ 
 
 
   delay(1); // stabiilsuseks 1ms
